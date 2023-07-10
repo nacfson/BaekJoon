f18a580f0676f6ba0c76f51b923c668a9c559cbe
Yoo Hyung Joo <65493803+nacfson@users.noreply.github.com>
2023-07-10 16:04:39 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 724 ms, Memory: 9716 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int compare(const void* a, const void* b)
+{
+	const int* x = (int*)a;
+	const int* y = (int*)b;
+
+	if (*x > *y)
+		return 1;
+	else if (*x < *y)
+		return -1;
+
+	return 0;
+}
+
+int main(void) {
+	int n;
+	cin >> n;
+
+	int arr[1000001];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	qsort(arr, n, sizeof(int), compare);
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << '\n';
+	}
+
+
+
+	return 0;
+}
