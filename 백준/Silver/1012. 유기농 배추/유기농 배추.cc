a53f40df169b1f8b676db97660fe39c724217b48
Yoo Hyung Joo <65493803+nacfson@users.noreply.github.com>
2023-09-11 12:31:35 +0900

    [Silver II] Title: 유기농 배추, Time: 4 ms, Memory: 2068 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int px[4]{ -1,1,0,0 };
+int py[4]{ 0,0,-1,1 };
+
+bool visited[100][100];
+int board[100][100];
+int t, m, n, k;
+
+void DFS(int x, int y){
+	visited[x][y] = true;
+	for (int i = 0; i < 4; i++) {
+		int nx = x + px[i];
+		int ny = y + py[i];
+		if (visited[nx][ny] == false && board[nx][ny] == 1 && nx >= 0 && ny >= 0 && nx < m && ny < n) {
+			DFS(nx, ny);
+		}
+	}
+}
+
+int main(void) {
+	queue<int> result;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		int answer = 0;
+		cin >> m >> n >> k;
+
+		fill_n(board[0], 100 * 100, 0);
+		fill_n(visited[0], 100 * 100, 0);
+
+		for (int j = 0; j  < k; j++) {
+			int tmp1, tmp2;
+			cin >> tmp1 >> tmp2;
+			board[tmp1][tmp2] = 1;
+		}
+
+		for (int j = 0; j < m; j++) {
+			for (int k = 0; k < n; k++) {
+				if (visited[j][k] == true || board[j][k] == 0) continue;
+				DFS(j, k);
+				++answer;
+			}
+		}
+		result.push(answer);
+	}
+
+	for(int i = 0; i < t; i++){
+		cout << result.front() << '\n';
+		result.pop();
+	}
+
+
+	return 0;
+}
